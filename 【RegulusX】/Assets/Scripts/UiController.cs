5542ee0bb32385912931e40a985f87b6bff32db4
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2018-05-28 23:11:40 +0800

    新增返回主選單按鈕
    1.返回按鈕在序章結束時出現
    2.發現BUG，若二次提水從水井後面提，人物會卡住

--- 【RegulusX】/Assets/Scripts/UiController.cs
+++ 【RegulusX】/Assets/Scripts/UiController.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Fungus;
 
 public class UiController : MonoBehaviour {
     public static Flowchart istalking;
+    public static Flowchart Mission;
     MissionController missionctor;
+    public GameObject EndButton;
 
     void Awake () {
-        //GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+        GameObject.Find("EndButton").SetActive(false);
         istalking = GameObject.Find("IsTalking").GetComponent<Flowchart>();
+        Mission = GameObject.Find("Mission").GetComponent<Flowchart>();
         MissionController missionctor = GetComponent<MissionController>();
     }
 
     // Update is called once per frame
     void Update () { 
         LockPlayer();
+        ContinuedButton();
         if (Input.GetKeyDown(KeyCode.O))
         {
             Flowchart.BroadcastFungusMessage("EscGameOperation");
@@ -24,6 +29,10 @@ public class UiController : MonoBehaviour {
     public static bool Talking
     {
         get { return istalking.GetBooleanVariable("IsTalking"); }
+    }
+    public static bool FirstChapterEnd
+    {
+        get { return Mission.GetBooleanVariable("End"); }
     }
 
     void LockPlayer()
@@ -36,6 +45,19 @@ public class UiController : MonoBehaviour {
         {
             GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
         }
+    }
+
+    void ContinuedButton()
+    {
+        if (UiController.FirstChapterEnd)
+        {
+            EndButton.SetActive(true);
+        }
+    }
+
+    public void ClickToMenu()
+    {
+        SceneManager.LoadScene("GameStart");
     }
 
 }
